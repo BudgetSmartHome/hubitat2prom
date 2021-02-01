1985add0f28bdd26304a85c27e71023cc5b4a520
Matthew Macdonald-Wallace <matt@mockingbirdconsulting.co.uk>
2021-02-01 07:37:21 +0000

    Move the attributes to a list

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ app = Flask(__name__)
 base_uri = os.getenv("HE_URI")
 access_token = os.getenv("HE_ACCESS_TOKEN")
 
+# This is the default set of metrics to be collected
+collected_metrics = [
+        "temperature",
+        "humidity",
+        "battery"
+        ]
 
 @app.route("/metrics")
 def metrics():
@@ -20,19 +26,9 @@ def metrics():
     for device in devices.json():
         device_details = requests.get(f"{base_uri}/{device['id']}?access_token={access_token}").json()
         for attrib  in device_details['attributes']:
-            if attrib["name"] == "humidity":
+            if attrib["name"] in collected_metrics:
                 device_attributes.append({
-                    "metric_name": f"humidity_{device_details['label'].lower().replace(' ','_')}_humidity",
-                    "metric_value": f"{attrib['currentValue']}",
-                    "metric_timestamp": time.time()})
-            if attrib["name"] == "battery":
-                device_attributes.append({
-                    "metric_name": f"battery_{device_details['label'].lower().replace(' ','_')}_charge_remaining",
-                    "metric_value": f"{attrib['currentValue']}",
-                    "metric_timestamp": time.time()})
-            if attrib["name"] == "temperature":
-                device_attributes.append({
-                    "metric_name": f"temperature_{device_details['label'].lower().replace(' ','_')}_current_value",
+                    "metric_name": f"{attrib['name']}_{device_details['label'].lower().replace(' ','_')}",
                     "metric_value": f"{attrib['currentValue']}",
                     "metric_timestamp": time.time()})
 
